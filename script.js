da49ab02edae39e8922d8508ae9d6c08d0c082fe
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2023-01-09 17:38:58 -0800

    face-api.min.js renamed, index.html updated with face-api.min.js script import, script.js updated to fix webcam error by following WebMd documentation

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
-const video = document.getElementById('video');
+// Utilizing Web MDN Documentation for MediaDevice getUserMedia functionality:
+// https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
 
-// connect to Webcam and start video
-function startVideo() {
-    navigator.mediaDevices.getUserMedia(
-        { video: {} },
-        stream => video.srcObject = stream,
-        err => console.error(err)
-    )
+const constraints = {
+    audio: true,
+    video: { width: 1280, height: 720 }
+  };
+
+function startVideo() { navigator.mediaDevices.getUserMedia(constraints)
+    .then((mediaStream) => {
+      const video = document.querySelector('video');
+      video.srcObject = mediaStream;
+      video.onloadedmetadata = () => {
+        video.play();
+      };
+    })
+    .catch((err) => {
+      // always check for errors at the end.
+      console.error(`${err.name}: ${err.message}`);
+    });
 }
 
-startVideo()
+startVideo();
