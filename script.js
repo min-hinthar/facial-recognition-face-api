9b0b9b3cd38db38ca342049a7568bf6528fc9f12
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2023-01-09 18:55:36 -0800

    index.html canvas position absolute added, script.js updated to use canvas and drawDetection, drawFaceLandmarks, drawFaceExpressions

--- script.js
+++ script.js
@@ -1,23 +1,38 @@
 // Utilizing Web MDN Documentation for MediaDevice getUserMedia functionality:
 // https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
 
-const constraints = {
-    audio: true,
-    video: { width: 1280, height: 720 }
-  };
+// get video element from index.html
+const video = document.getElementById('video');
 
-function startVideo() { navigator.mediaDevices.getUserMedia(constraints)
-    .then((mediaStream) => {
-      const video = document.querySelector('video');
-      video.srcObject = mediaStream;
-      video.onloadedmetadata = () => {
-        video.play();
-      };
-    })
-    .catch((err) => {
-      // always check for errors at the end.
-      console.error(`${err.name}: ${err.message}`);
-    });
-}
+// use constructor to load models {important to add ./!}
+Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri('./models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('./models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('./models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('./models')
+]).then(startVideo);
 
-startVideo();
+// create function to start webcam video rendering
+function startVideo() {
+  navigator.getUserMedia(
+    { video: {} },
+    stream => video.srcObject = stream,
+    err => console.error(err)
+  )
+};
+
+// use canvas and append to html using faceapi to draw face detection, face landmarks and face expressions 
+video.addEventListener('play', () => {
+  const canvas = faceapi.createCanvasFromMedia(video);
+  document.body.append(canvas);
+  const displaySize = { width: video.width, height: video.height };
+  faceapi.matchDimensions(canvas, displaySize);
+  setInterval(async () => {
+    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+    const resizedDetections = faceapi.resizeResults(detections, displaySize)
+    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+    faceapi.draw.drawDetections(canvas, resizedDetections)
+    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+    faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+  }, 100);
+});
